623b6d93c3a7cdeccbc73f2ab1a47fbf638a7ba9
Max Morgan <maxmmorgan93@gmail.com>
2022-12-29 13:45:59 -0600

    initial commit

---
+++ FizzBuzz1/FizzBuzz1/Fizz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz1
+{
+    internal class Fizz
+    {
+        public static string FizzBuzz(int x)
+        {
+            string y = "fizz";
+            string z = "buzz";
+            string m = "fizzbuzz";
+
+            if (x % 15 == 0)
+            {
+                Console.WriteLine(m);
+                return m;
+            }
+            else if (x % 5 == 0)
+            {
+                Console.WriteLine(z);
+                return z;
+            }
+            else if (x % 3 == 0)
+            {
+                Console.WriteLine(z);
+                return z;
+            }
+            else
+            {
+                Console.WriteLine(x);
+                return x.ToString();
+
+            }
+        }
+    }
+}
---
+++ FizzBuzz1/FizzBuzz1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FizzBuzz1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fizz.FizzBuzz(10);
+            
+        }
+
+         
+    }
+}
